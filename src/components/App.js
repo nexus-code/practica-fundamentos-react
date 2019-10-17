64a326cab977e8f4572c77f17478bb42d642cc46
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2019-10-17 13:35:56 +0200

    Practice began. App empty

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; // precisa instalar dependencias
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
