eb5db522864dadea055984a727734bf49656c44f
Miguel Angel Cardenas <ma.cardenas@nexuscode.com>
2019-10-17 14:58:22 +0200

    Restore App at root

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { UserContext, UserProvider } from './context/UserContext'
 // import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; // precisa instalar dependencias
 
 
